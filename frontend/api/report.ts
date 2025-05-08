f9f3bb0beba1fff8e1fa57f7963d64420da56061
Samuel Espinosa <saespinosab@unal.edu.co>
2025-05-08 10:04:18 -0500

    vercel types

--- frontend/api/report.ts
+++ frontend/api/report.ts
@@ -1,26 +1,23 @@
+
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import axios from 'axios';
 
-const REPORT_API_URL = process.env.REPORT_API_URL!;
+const BACKEND_URL = 'http://ec2-18-226-133-140.us-east-2.compute.amazonaws.com:3002/api/reports';
 
-export default async function handler(req, res) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     if (req.method === 'GET') {
       const { url } = req.query;
-      console.log('awaiting');
-      const response = await axios.get(REPORT_API_URL, { params: { url } });
-      console.log(response);
-      return res.status(200).json(response.data);
+      const response = await axios.get(BACKEND_URL, { params: { url } });
+      res.status(200).json(response.data);
+    } else if (req.method === 'POST') {
+      const response = await axios.post(BACKEND_URL, req.body);
+      res.status(200).json(response.data);
+    } else {
+      res.status(405).end('Method Not Allowed');
     }
-
-    if (req.method === 'POST') {
-      console.log('awaiting');
-      const response = await axios.post(REPORT_API_URL, req.body);
-      console.log(response)
-      return res.status(200).json(response.data);
-    }
-
-    res.status(405).json({ error: 'Method not allowed' });
-  } catch (err) {
-    res.status(500).json({ error: 'Server error' });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to connect to backend' });
   }
 }
